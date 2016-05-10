eea7f4b2e889a92e39e7f6139ca699c997964f51
pierreglardon <pierreglardoncontact@gmail.com>
2016-05-10 10:23:48 +0200

    fix

--- js/pierreglardon.js
+++ js/pierreglardon.js
@@ -4,34 +4,25 @@ $( document ).ready(function() {
     var parallax = new Parallax(scene);
 
     // Parallax portrait
-    // init controller
     var controllerDiv = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "0.5", duration: "150%"}});
-    // build scenes
     new ScrollMagic.Scene({triggerElement: "#portrait"})
-    .setTween("#portrait > div", {y: "80%", ease: Linear.easeNone})
+    .setTween("#portrait > div", {y: "100%", ease: Linear.easeNone})
     .addTo(controllerDiv);
 
     // Scrolling
-    // init controller
     var controllerScroll = new ScrollMagic.Controller();
-    // build tween
     var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
-    // build scene
     var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 100, triggerHook: "onLeave"})
                     .setTween(tween)
                     .addTo(controllerScroll);
-    // change behaviour of controller to animate scroll instead of jump
     controllerScroll.scrollTo(function (newpos) {
         TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
     });
-    //  bind scroll to anchor links
     $(document).on("click", "a[href^='#']", function (e) {
         var id = $(this).attr("href");
         if ($(id).length > 0) {
             e.preventDefault();
-            // trigger scroll
             controllerScroll.scrollTo(id);
-            // if supported by the browser we can even update the URL.
             if (window.history && window.history.pushState) {
                 history.pushState("", document.title, id);
             }

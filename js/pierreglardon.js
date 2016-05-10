c706930ea382de7af9ed7da672c86e5046f5bc7c
pierreglardon <pierreglardoncontact@gmail.com>
2016-05-10 12:33:29 +0200

    responsive 1

--- js/pierreglardon.js
+++ js/pierreglardon.js
@@ -9,35 +9,10 @@ $( document ).ready(function() {
     .setTween("#portrait > div", {y: "100%", ease: Linear.easeNone})
     .addTo(controllerDiv);
 
-    // Scrolling
-    var controllerScroll = new ScrollMagic.Controller();
-    var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
-    var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 100, triggerHook: "onLeave"})
-                    .setTween(tween)
-                    .addTo(controllerScroll);
-    controllerScroll.scrollTo(function (newpos) {
-        TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+    // responsive menu
+    var mobileMenu  = $('nav.navigationMobile ul'),
+        openMenu    = $('nav.navigationMobile .openMenu');
+    openMenu.click(function() {
+        mobileMenu.slideToggle();
     });
-    $(document).on("click", "a[href^='#']", function (e) {
-        var id = $(this).attr("href");
-        if ($(id).length > 0) {
-            e.preventDefault();
-            controllerScroll.scrollTo(id);
-            if (window.history && window.history.pushState) {
-                history.pushState("", document.title, id);
-            }
-        }
-    });
-    var controller2 = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "0.2", duration: "100%"}});
-    new ScrollMagic.Scene({triggerElement: "#home"})
-                    .setClassToggle(".navlink.l1", "active")
-                    .addTo(controller2);
-    new ScrollMagic.Scene({triggerElement: "#aboutme"})
-                    .setClassToggle(".navlink.l2", "active")
-                    .addTo(controller2);
-    new ScrollMagic.Scene({triggerElement: "#portfolio"})
-                    .setClassToggle(".navlink.l3", "active")
-                    .addTo(controller2);
-
-
 });
